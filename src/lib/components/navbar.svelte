5a205c720cdf26e70639dee416541ff3ef065e59
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-04-28 22:31:40 +0200

    navbar wip

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,19 +1,21 @@
+<script lang="ts">
+  import { WebsiteName } from "~/config";
+
+</script>
+
 <div class="navbar bg-base-100 container mx-auto">
   <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl" href="/">SaaS Starter</a>
+    <a class="btn btn-ghost normal-case text-xl" href="/">{WebsiteName}</a>
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1 hidden sm:flex font-bold text-lg">
       <li class="md:mx-4">
-        <a
-          href="https://github.com/CriticalMoments/CMSaasStarter"
-          class="border border-primary">★ us on Github</a
-        >
+        <a href="/login/sign_up" class="border border-primary">Sign up</a>
       </li>
-      <li class="md:mx-2"><a href="/blog">Blog</a></li>
       <li class="md:mx-2"><a href="/pricing">Pricing</a></li>
       <li class="md:mx-2"><a href="/account">Account</a></li>
     </ul>
+
     <div class="dropdown dropdown-end sm:hidden">
       <!-- svelte-ignore a11y-label-has-associated-control -->
       <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
@@ -37,16 +39,13 @@
         tabindex="0"
         class="menu menu-lg dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-bold"
       >
-        <li><a href="/blog">Blog</a></li>
         <li><a href="/pricing">Pricing</a></li>
         <li><a href="/account">Account</a></li>
-        <li>
-          <a
-            href="https://github.com/CriticalMoments/CMSaasStarter"
-            class="border border-primary">★ us on Github</a
-          >
+        <li class="md:mx-4">
+          <a href="/login/sign_up" class="border border-primary">Sign up</a>
         </li>
       </ul>
     </div>
+
   </div>
 </div>
--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -20,7 +20,6 @@
       <span class="footer-title opacity-80">Explore</span>
       <a class="link link-hover mb-1" href="/">Overview</a>
       <a class="link link-hover my-1" href="/pricing">Pricing</a>
-      <a class="link link-hover my-1" href="/blog">Blog</a>
       <a class="link link-hover my-1" href="/contact_us">Contact Us</a>
       <a
         class="link link-hover my-1"
