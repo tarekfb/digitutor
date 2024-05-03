3862defd57e2312f05865ebd8e09210e95fee9bb
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-05-03 21:00:42 +0200

    responsive sidebar

--- src/routes/(admin)/account/(menu)/+layout.svelte
+++ src/routes/(admin)/account/(menu)/+layout.svelte
@@ -177,14 +177,17 @@
 
 <div class="space-y-6 p-10 pb-16 md:block">
   <div class="space-y-0.5">
-    <h2 class="text-2xl font-bold tracking-tight">Settings</h2>
+    <h2 class="text-2xl font-bold tracking-tight">Dashboard</h2>
     <p class="text-muted-foreground">
-      Manage your account settings and set e-mail preferences.
+      Översikt på lektioner, meddelanden och din.
     </p>
   </div>
   <Separator class="my-6" />
-  <div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-    <aside class="-mx-4 lg:w-1/5">
+  <div
+    class="flex flex-row space-x-12 space-y-0 sm:flex-col sm:space-y-8 sm:space-x-0"
+  >
+    <aside class="-mx-4 lg:w-1/5 overflow-x-hidden bg-slate-200">
+      <!-- add toggle button and pass it to component -->
       <SidebarNav items={sidebarNavItems} />
     </aside>
     <div class="flex-1 lg:max-w-2xl">
