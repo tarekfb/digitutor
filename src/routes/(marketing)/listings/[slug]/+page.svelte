91ca2517a4ef158d7ba17c6c1268978df02e9abc
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-05-08 09:59:51 +0200

    wip for dropdown

--- src/routes/(marketing)/listings/[slug]/+page.svelte
+++ src/routes/(marketing)/listings/[slug]/+page.svelte
@@ -12,6 +12,16 @@
   import { Subjects } from "src/lib/models/common.js";
   import { Plus } from "lucide-svelte";
   import { CircleMinus } from "lucide-svelte";
+  import { tick } from "svelte";
+  import { Check } from "lucide-svelte";
+  import { ChevronsUpDown } from "lucide-svelte";
+  import { browser } from "$app/environment";
+  import { page } from "$app/stores";
+  import * as Form from "$lib/components/ui/form/index.js";
+  import * as Popover from "$lib/components/ui/popover/index.js";
+  import * as Command from "$lib/components/ui/command/index.js";
+  import { cn } from "$lib/utils.js";
+  import { buttonVariants } from "$lib/components/ui/button/index.js";
 
   export let data;
   const { listing, user, form } = data;
@@ -41,6 +51,48 @@
     },
   });
   const { form: formData, enhance, errors } = listingForm;
+
+  const languages = [
+    { label: "JavaScript", value: "1" },
+    { label: "Python", value: "2" },
+    { label: "Java", value: "3" },
+    { label: "CSharp", value: "4" },
+    { label: "CPlusPlus", value: "5" },
+    { label: "Ruby", value: "6" },
+    { label: "Swift", value: "7" },
+    { label: "Go", value: "8" },
+    { label: "PHP", value: "9" },
+    { label: "TypeScript", value: "10" },
+    { label: "HTML_CSS", value: "11" },
+    { label: "ReactJS", value: "12" },
+    { label: "AngularJS", value: "13" },
+    { label: "VueJS", value: "14" },
+    { label: "NodeJS", value: "15" },
+    { label: "Django", value: "16" },
+    { label: "Flask", value: "17" },
+    { label: "Spring", value: "18" },
+    { label: "dotNet", value: "19" },
+    { label: "Laravel", value: "20" },
+  ];
+
+  let open = false;
+  let value = "";
+
+  $: selectedValue =
+    languages.find((lang) => lang.value === value)?.label ??
+    "Select a framework...";
+
+  // We want to refocus the trigger button when the user selects
+  // an item from the list so users can continue navigating the
+  // rest of the form with the keyboard.
+  function closeAndFocusTrigger(triggerId: string) {
+    open = false;
+    tick().then(() => {
+      document.getElementById(triggerId)?.focus();
+    });
+  }
+
+  
 </script>
 
 <div class="p-8 space-y-2">
@@ -104,7 +156,45 @@
 
           {#if isEditing.subjects}
             <div class="flex justify-between items-center">
-              <p>dropdown</p>
+              <Popover.Root bind:open let:ids>
+                <Popover.Trigger asChild let:builder>
+                  <Button
+                    builders={[builder]}
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    class="w-[200px] justify-between"
+                  >
+                    {selectedValue}
+                    <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                  </Button>
+                </Popover.Trigger>
+                <Popover.Content class="w-[200px] p-0">
+                  <Command.Root>
+                    <Command.Input placeholder="Search framework..." />
+                    <Command.Empty>No framework found.</Command.Empty>
+                    <Command.Group>
+                      {#each languages as language}
+                        <Command.Item
+                          value={language.value}
+                          onSelect={(currentValue) => {
+                            value = currentValue;
+                            closeAndFocusTrigger(ids.trigger);
+                          }}
+                        >
+                          <Check
+                            class={cn(
+                              "mr-2 h-4 w-4",
+                              value !== language.value && "text-transparent",
+                            )}
+                          />
+                          {language.label}
+                        </Command.Item>
+                      {/each}
+                    </Command.Group>
+                  </Command.Root>
+                </Popover.Content>
+              </Popover.Root>
 
               <div class="flex justify-end gap-x-4 w-full">
                 <Button
