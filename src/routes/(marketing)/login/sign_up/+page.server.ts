7b2bae696ada74e40e3111792f27e88c673ddfe8
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-05-17 15:32:39 +0200

    spelling and seterror instead of message

--- src/routes/(marketing)/login/sign_up/+page.server.ts
+++ src/routes/(marketing)/login/sign_up/+page.server.ts
@@ -1,7 +1,7 @@
 import { error, fail, redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import { genericErrorMessage, unknownErrorMessage } from "src/lib/constants";
-import { message, superValidate } from "sveltekit-superforms";
+import { message, setError, superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { signUpSchema, type CreateProfile } from "src/lib/models/user";
 import { createProfile } from "src/lib/server/database/profiles";
@@ -62,8 +62,7 @@ export const actions = {
 
             // https://github.com/orgs/supabase/discussions/1282
             if (data.user.identities && data.user.identities.length === 0)
-                return message(form, { variant: "destructive", title: "E-postadressen används redan", description: "Testa att skapa ett konto med annan e-postadress." }, { status: 400 });
-
+                return setError(form, "email", "E-postadressen används redan");
 
             inputUser = {
                 id: data.user.id,
@@ -78,7 +77,7 @@ export const actions = {
 
         try {
             await createProfile(supabase, inputUser)
-            return message(form, { variant: "success", title: "Verifiera e-postadress", description: "Kika in din inkorg för att verifiera e-posten.", status: 201 });
+            return message(form, { variant: "success", title: "Verifiera e-postadress", description: "Kika i din inkorg för att verifiera e-posten.", status: 201 });
         } catch (error) {
             console.error("Error when creating profile", error);
             return message(form, genericErrorMessage, { status: 500 });
