9858d2e4641550d222cfff446ecea6fcace57e63
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-06-07 19:33:40 +0200

    remove unused code

--- src/routes/(marketing)/login/forgot_password/+page.svelte
+++ src/routes/(marketing)/login/forgot_password/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { Auth } from "@supabase/auth-ui-svelte";
-  import { sharedAppearance, oauthProviders } from "../login_config";
 
   export let data;
 </script>
@@ -10,7 +8,7 @@
 </svelte:head>
 
 <h1 class="text-2xl font-bold mb-6">Forgot Password</h1>
-<Auth
+<!-- <Auth
   supabaseClient={data.supabase}
   view="forgotten_password"
   redirectTo={`${data.url}/auth/callback?next=%2Faccount%2Fsettings%2Freset_password`}
@@ -19,8 +17,11 @@
   showLinks={false}
   appearance={sharedAppearance}
   additionalData={undefined}
-/>
-<div class="text-l text-slate-800 mt-4">
+/> -->
+
+<!-- <div class="text-l text-slate-800 mt-4">
   Remember your password? <a class="underline" href="/login/sign_in">Sign in</a
-  >.
-</div>
+    >.
+  </div> -->
+
+<h1 class="text-4xl">Not implemented</h1>
