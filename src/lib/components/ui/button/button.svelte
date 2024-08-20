46ac497b1d1d5fef9fc665705217871c7d18e4ba
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-08-20 12:09:50 +0200

    tips: not implemented

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -15,7 +15,10 @@
 
 <ButtonPrimitive.Root
   {builders}
-  class={cn(buttonVariants({ variant, size, className }), "tracking-wider uppercase")}
+  class={cn(
+    "tracking-wider uppercase",
+    buttonVariants({ variant, size, className }),
+  )}
   type="button"
   {...$$restProps}
   on:click
