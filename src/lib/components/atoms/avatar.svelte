afd28a7019b3b1e77dac51c0db6000e178f52236
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-09-18 11:45:28 +0200

    impl by other pr

--- src/lib/components/atoms/avatar.svelte
+++ src/lib/components/atoms/avatar.svelte
@@ -31,7 +31,6 @@
     variant="ghost"
     data-test-id="avatar"
     class={cn(`relative rounded-full`, className)}
-    data-test-id="avatar"
     on:click={onClick}
   >
     <Avatar.Root class={base}>
