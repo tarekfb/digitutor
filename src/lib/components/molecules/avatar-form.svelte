7a0870b2f5f616352edbabdd8c00cf3f56a93bbf
tarekfb <56593098+tarekfb@users.noreply.github.com>
2024-10-04 18:16:45 +0200

    Update avatar-form.svelte
    Missing tag

--- src/lib/components/molecules/avatar-form.svelte
+++ src/lib/components/molecules/avatar-form.svelte
@@ -74,7 +74,8 @@ import * as Form from "$lib/components/ui/form";
           accept={getMimeType()}
           class="overflow-hidden flex h-10 w-full border border-input rounded-md bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
           on:input={(e) => setAvatar(e)}
-      </div>
+      />
+</div>
     {:else}
       <Dropzone
         on:drop={handleFilesSelect}
