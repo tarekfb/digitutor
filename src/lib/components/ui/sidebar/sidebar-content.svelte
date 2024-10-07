ae06c7c258479737b5391a773ba1053c85dca8f8
tarekfb <56593098+tarekfb@users.noreply.github.com>
2024-10-07 15:21:51 +0200

    Update sidebar-content.svelte
    Remove swipable indicator

--- src/lib/components/ui/sidebar/sidebar-content.svelte
+++ src/lib/components/ui/sidebar/sidebar-content.svelte
@@ -7,35 +7,18 @@
 
   let className: $$Props["class"] = undefined;
   export { className as class };
-  const swipable = className?.includes("swipable");
+  const mobile = className?.includes("mobile");
 </script>
 
 <DrawerPrimitive.Portal>
   <DrawerOverlay />
-  {#if swipable}
     <DrawerPrimitive.Content
       class={cn(
-        "fixed inset-y-0 right-0 z-50 ml-24 grid grid-cols-6 h-full rounded-l-[10px] border bg-background",
-        className,
-      )}
-      {...$$restProps}
-    >
-      <div
-        class="my-auto mx-0.5 md:mx-4 w-2 h-20 rounded-full bg-muted-foreground/30 justify-self-start col-start-1"
-      ></div>
-      <div class="flex flex-col gap-y-4 col-start-2 col-span-4">
-        <slot />
-      </div>
-    </DrawerPrimitive.Content>
-  {:else}
-    <DrawerPrimitive.Content
-      class={cn(
-        "fixed inset-y-0 right-0 z-50 ml-24 flex flex-col h-full rounded-l-[10px] border bg-background",
+        {mobile ? "fixed inset-y-0 right-0 z-50 ml-24 flex h-full rounded-l-[10px] border bg-background" : "fixed inset-y-0 right-0 z-50 ml-24 flex flex-col h-full rounded-l-[10px] border bg-background"},
         className,
       )}
       {...$$restProps}
     >
       <slot />
     </DrawerPrimitive.Content>
-  {/if}
 </DrawerPrimitive.Portal>
