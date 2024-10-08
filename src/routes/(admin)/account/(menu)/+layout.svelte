673172d59777ad9bae4eabb69228668ceac97f39
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-10-08 16:58:02 +0200

    remove admin section stuff not needed

--- src/routes/(admin)/account/(menu)/+layout.svelte
+++ src/routes/(admin)/account/(menu)/+layout.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { writable } from "svelte/store";
-  import { setContext } from "svelte";
   import Sidebar from "src/lib/components/organisms/sidebar.svelte";
   import type { PageData } from "./$types";
   import { logout } from "src/lib/utils";
@@ -8,13 +6,6 @@
 
   export let data: PageData;
   $: ({ supabase, session, profile } = data);
-
-  const adminSectionStore = writable("");
-  setContext("adminSection", adminSectionStore);
-  let adminSection: string;
-  adminSectionStore.subscribe((value) => {
-    adminSection = value;
-  });
 </script>
 
 <Navbar profile={false} logout={false}>
