2a5c75b9e0e38183d8d30de192eca73452faa1a2
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-10-16 11:58:03 +0200

    created root container which enables escaping layout

---
+++ src/lib/components/molecules/root-container.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { cn } from "src/lib/utils";
+
+  let className: string | null | undefined = undefined;
+  export { className as class };
+</script>
+
+<div class={cn("flex flex-col items-center m-8 gap-y-4", className)}>
+  <slot />
+</div>
