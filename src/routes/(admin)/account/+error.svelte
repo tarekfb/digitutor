33fcc89ca76eceb51d045f20e323ce5ed56bf412
Tarek Bermalm <tarek.bermalm@gmail.com>
2024-11-01 10:49:08 +0100

    include navbar for account error

--- src/routes/(admin)/account/+error.svelte
+++ src/routes/(admin)/account/+error.svelte
@@ -1,6 +1,8 @@
 <script>
   import { page } from "$app/stores";
   import Error from "$lib/components/organisms/error.svelte";
+  import Navbar from "src/lib/components/organisms/navbar.svelte";
 </script>
 
+<Navbar profile={false} logout={false} />
 <Error error={$page.error} code={$page.status} />
