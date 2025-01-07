262901915356e57a3d08f44942b845f4980e8012
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-01-07 16:20:25 +0100

    styling for rootcontainer

--- src/lib/components/templates/root-container.svelte
+++ src/lib/components/templates/root-container.svelte
@@ -5,13 +5,15 @@
   export { className as class };
   export let responsiveGap = false;
   export let maxWidth = false;
+  export let minWidth = false;
 </script>
 
 <div
   class={cn(
     "m-8 flex flex-col items-center",
     responsiveGap ? "gap-y-2 md:gap-y-4" : "gap-y-4",
-    maxWidth ? "md:max-w-screen-sm lg:max-w-screen-md" : "",
+    maxWidth ? "md:max-w-screen-xl lg:max-w-screen-md" : "",
+    minWidth ? "min-w-full px-8 md:min-w-[400px] md:px-0 lg:min-w-[500px]" : "",
     className,
   )}
 >
