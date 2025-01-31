e3986d04ad27ad768151bf88f56d7e8aa83ca3c0
tarekfb <56593098+tarekfb@users.noreply.github.com>
2025-01-31 11:40:16 +0100

    Update hooks.client.ts
    Update import to sveltekit

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,7 +2,7 @@
 import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
 // or
 // import { clientInit } from '@jill64/sentry-sveltekit-cloudflare'
-import * as Sentry from "@sentry/svelte";
+import * as Sentry from "@sentry/sveltekit";
 
 const onError = init(
     'https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112'
