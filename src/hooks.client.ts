94d3774c8b06ade3b864f00897a235857ae1a8e3
tarekfb <56593098+tarekfb@users.noreply.github.com>
2025-01-31 13:14:41 +0100

    Update hooks.client.ts
    Env identifier

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,4 +1,4 @@
-// hooks.client.js
+import { PUBLIC_ENVIRONMENT } from "$env/static/public"
 import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
 // or
 // import { clientInit } from '@jill64/sentry-sveltekit-cloudflare'
@@ -6,13 +6,14 @@ import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
 
 const onError = init(
     'https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112'
-    // ,
-    // {
-    //   sentryOptions: {
+    ,
+    {
+    sentryOptions: {
+        environment: PUBLIC_ENVIRONMENT 
     //     // ... Other Sentry Config
     //   },
     //   enableInDevMode: boolean (default: false)
-    // }
+    }
 )
 
 export const handleError = onError((e, sentryEventId) => {
