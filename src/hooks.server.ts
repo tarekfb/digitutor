676d1472f7ff59b39bb57db56039ddd3ad0b03bf
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-01-31 18:54:45 +0100

    fix for sentry in hooks server ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -92,27 +92,25 @@ const authGuard: Handle = async ({ event, resolve }) => {
 
 
 const { onHandle, onError } = init(
-  'https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112'
-   ,
+  'https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112',
   {
-    environment: PUBLIC_ENVIRONMENT
-  //   toucanOptions: {
-  //     // ... Other Sentry Config
-  //   },
-  //   handleOptions: {
-  //     handleUnknownRoutes: boolean (default: false)
-  //   },
-  //   enableInDevMode: boolean (default: false)
- }
+    toucanOptions: {
+      environment: PUBLIC_ENVIRONMENT
+    },
+    //   handleOptions: {
+    //     handleUnknownRoutes: boolean (default: false)
+    //   },
+    //   enableInDevMode: boolean (default: false)
+  }
 )
 
 export const handle: Handle = sequence(supabase, authGuard);
+
+export const handleError = onError((e, sentryEventId) => {
+  console.error(e, sentryEventId)
+})
 
 // This func is not used but comes from https://github.com/jill64/sentry-sveltekit-cloudflare
 // export const handle = onHandle(({ event, resolve }) => {
 //   // Your Handle Code
 // })
-
-export const handleError = onError((e, sentryEventId) => {
-  console.error(e, sentryEventId)
-})
