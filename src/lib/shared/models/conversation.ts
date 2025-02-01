c44780c697fd68417286d010b4de642a572b8575
tarekfb <56593098+tarekfb@users.noreply.github.com>
2025-02-01 19:37:39 +0100

    Update conversation.ts

--- src/lib/shared/models/conversation.ts
+++ src/lib/shared/models/conversation.ts
@@ -55,10 +55,9 @@ export const formatMessage = ({
 export const sendMessageSchema = z.object({
   content: z
     .string()
+    .trim()
     .min(1, "Måste vara minst 1 karaktär.")
-    .max(300, "Får inte vara mer än 300 karaktärer.")
-    .refine(m => m.trim, "Får inte vara tomt.")
-
+    .max(300, "Får inte vara mer än 300 karaktärer."),
 });
 
 const requestContactProperties = {
