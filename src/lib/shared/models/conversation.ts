f495e0ab602db2673d7f15513df08776e1a8fa73
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-02-04 19:32:07 +0100

    fix message checking

--- src/lib/shared/models/conversation.ts
+++ src/lib/shared/models/conversation.ts
@@ -55,10 +55,9 @@ export const formatMessage = ({
 export const sendMessageSchema = z.object({
   content: z
     .string()
-    .min(1, "Måste vara minst 1 karaktär.")
-    .max(300, "Får inte vara mer än 300 karaktärer.")
-    .refine(m => !m.trim, "Får inte vara tomt.")
-
+    .trim()
+    .min(1, "Meddelandet måste vara minst 1 karaktär.")
+    .max(300, "Meddelandet får inte vara mer än 300 karaktärer.")
 });
 
 const requestContactProperties = {
