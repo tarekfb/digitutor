f4fbdc24c72ebf926be0bda8614f2112ee51b626
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-02-24 16:53:34 +0100

    remove sentry in client side

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,12 +1,12 @@
-import * as Sentry from '@sentry/sveltekit';
-import { initCloudflareSentryHandle, sentryHandle } from '@sentry/sveltekit';
-import { sequence } from '@sveltejs/kit/hooks';
+// import * as Sentry from '@sentry/sveltekit';
+// import { initCloudflareSentryHandle, sentryHandle } from '@sentry/sveltekit';
+// import { sequence } from '@sveltejs/kit/hooks';
 
-export const handle = sequence(
-    initCloudflareSentryHandle({
-        dsn: "https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112",
-        tracesSampleRate: 1.0,
-        integrations: [Sentry.replayIntegration()],
-    }),
-    sentryHandle(),
-);
+// export const handle = sequence(
+//     initCloudflareSentryHandle({
+//         dsn: "https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112",
+//         tracesSampleRate: 1.0,
+//         integrations: [Sentry.replayIntegration()],
+//     }),
+//     sentryHandle(),
+// );
