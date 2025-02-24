9ecd8f81c739747300f19559ed242ecc7b16ef08
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-02-24 16:49:27 +0100

    clientside sentry setup

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,21 +1,12 @@
-// import { PUBLIC_ENVIRONMENT } from "$env/static/public";
-// import { init } from "@jill64/sentry-sveltekit-cloudflare/client";
-// // or
-// // import { clientInit } from '@jill64/sentry-sveltekit-cloudflare'
-// // import * as Sentry from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+import { initCloudflareSentryHandle, sentryHandle } from '@sentry/sveltekit';
+import { sequence } from '@sveltejs/kit/hooks';
 
-// const onError = init(
-//   "https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112",
-//   {
-//     sentryOptions: {
-//       environment: PUBLIC_ENVIRONMENT,
-//       //     // ... Other Sentry Config
-//       //   },
-//       //   enableInDevMode: boolean (default: false)
-//     },
-//   },
-// );
-
-// export const handleError = onError((e, sentryEventId) => {
-//   console.error(e, sentryEventId);
-// });
+export const handle = sequence(
+    initCloudflareSentryHandle({
+        dsn: "https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112",
+        tracesSampleRate: 1.0,
+        integrations: [Sentry.replayIntegration()],
+    }),
+    sentryHandle(),
+);
