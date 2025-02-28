ee58cf1d6a677bed41363caa2ecbb7953d5e8074
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-02-28 16:50:04 +0100

    sentry upgrasde

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-// import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { sentrySvelteKit } from "@sentry/sveltekit";
+import { defineConfig, loadEnv } from 'vite';
 // import { svelteInspector } from '@sveltejs/vite-plugin-svelte-inspector';
-import { defineConfig  } from 'vite';
-// ,loadEnv
-// const env = loadEnv(
-//   'all',
-//   process.cwd()
-// );
+
+const env = loadEnv(
+  'all',
+  process.cwd()
+);
 
 export default defineConfig({
   plugins: [
-    // sentrySvelteKit(
-    //   {
-    //     sourceMapsUploadOptions: {
-    //       org: 'digitutor-dev',
-    //       project: 'javascript-sveltekit',
-    //       authToken: env.VITE_PRIVATE_SENTRY_AUTH_TOKEN,
-    //     }
-    //   }
-    // ),
+    sentrySvelteKit(
+      {
+        sourceMapsUploadOptions: {
+          org: 'digitutor-dev',
+          project: 'javascript-sveltekit',
+          authToken: env.VITE_PRIVATE_SENTRY_AUTH_TOKEN,
+        }
+      }
+    ),
     sveltekit(),
     // svelteInspector({
     //   /* plugin options */
