46278f260c31a9983dcbc54d332edcec77b6c67c
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-03-05 10:55:11 +0100

    fix and improve tests

--- src/lib/components/molecules/signup-content.svelte
+++ src/lib/components/molecules/signup-content.svelte
@@ -18,7 +18,7 @@
 </script>
 
 <div class="mb-4 space-y-1 text-center lg:text-start">
-  <PrimaryTitle>Skapa konto som {getReadableType(type)}</PrimaryTitle>
+  <PrimaryTitle testId={type}>Skapa konto som {getReadableType(type)}</PrimaryTitle>
   <p class="text-muted-foreground">
     Ett konto är gratis och inget betalkort behövs. Har du redan ett konto?
     <a href="/sign-in" class="underline"> Logga in här. </a>
