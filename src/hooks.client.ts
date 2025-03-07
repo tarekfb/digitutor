bfe0f131a9ec92c3dc4d545728c3465feec109d2
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-03-07 09:51:14 +0100

    add client hooks func handlerrorwithsentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,6 @@
 import { PUBLIC_ENVIRONMENT } from '$env/static/public';
 import * as Sentry from '@sentry/sveltekit';
+import { handleErrorWithSentry } from '@sentry/sveltekit';
 
 Sentry.init({
     dsn: 'https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112',
@@ -11,3 +12,8 @@ Sentry.init({
     replaysOnErrorSampleRate: 1.0,
     integrations: [Sentry.replayIntegration()],
 });
+
+
+export const handleError = handleErrorWithSentry();
+// or alternatively, if you don't have a custom error handler:
+// export const handleError = handleErrorWithSentry();
