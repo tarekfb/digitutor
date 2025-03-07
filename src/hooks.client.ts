0a84ec171f858e2e4d995c5f9aa0e63458aa0e7a
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-03-07 09:48:56 +0100

    added env to clietn and server

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,10 +1,13 @@
+import { PUBLIC_ENVIRONMENT } from '$env/static/public';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-  dsn: 'https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112',
-  tracesSampleRate: 1.0,
-  // For instance, initialize Session Replay:
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  integrations: [Sentry.replayIntegration()],
+    dsn: 'https://485a49edf664c4bad08c2ab0bf87a8eb@o4507622077169664.ingest.de.sentry.io/4507622079660112',
+    environment: PUBLIC_ENVIRONMENT,
+    enabled: PUBLIC_ENVIRONMENT !== "local",
+    tracesSampleRate: 1.0,
+    // For instance, initialize Session Replay:
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    integrations: [Sentry.replayIntegration()],
 });
