7d37d3497cf2c6c74f0ff2ed7003ce93a4ec40f3
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-03-09 16:40:40 +0100

    progress for hiding credits on pricing page and billing

--- src/routes/(marketing)/pricing/+page.svelte
+++ src/routes/(marketing)/pricing/+page.svelte
@@ -4,6 +4,7 @@
   import Container from "src/lib/components/templates/container.svelte";
   import Button from "src/lib/components/ui/button/button.svelte";
   import {
+    freeCredits,
     premiumPlan,
     pricingPlans,
     websiteName,
@@ -13,6 +14,7 @@
   import SecondaryTitle from "src/lib/components/atoms/secondary-title.svelte";
   import { costPerRequest } from "src/lib/shared/constants/constants.js";
   import ArrowRightIcon from "lucide-svelte/icons/arrow-right";
+  import Link from "src/lib/components/atoms/link.svelte";
 
   export let currentPlanId: string = "";
 </script>
@@ -39,7 +41,7 @@
   </div>
 
   <section
-    class="flex flex-col-reverse flex-wrap w-full place-content-center gap-6 md:gap-10 lg:flex-row"
+    class="flex w-full flex-col-reverse flex-wrap place-content-center gap-6 md:gap-10 lg:flex-row"
   >
     {#each pricingPlans as pricingPlan}
       <PricingModule
@@ -63,11 +65,15 @@
         >
       </Accordion.Item>
       <Accordion.Item value="item-2">
-        <Accordion.Trigger>Hur funkar krediter?</Accordion.Trigger>
+        <Accordion.Trigger
+          >Hur funkar gratis kontaktförfrågningar?</Accordion.Trigger
+        >
         <Accordion.Content>
-          Varje gång du skapar en kontakt med en lärare kostar det {costPerRequest}
-          krediter. Om du redan har en konversation och chatt med läraren kostar
-          meddelanden ingenting. Du kan se hur många krediter du har under
+          När du registrerar dig får du automatiskt {freeCredits} gratis kontaktförfrågningar.
+          När du sedan kontaktar en lärare förbrukas en av dessa. Om du redan har
+          en konversation och chatt med en lärare kan du såklart skicka hur många
+          meddelanden du vill till denna lärare. Du kan se hur många återstående
+          kontaktförfrågningar du har under
           <a href="/account/billing" class="link">betalningar</a>.
         </Accordion.Content>
       </Accordion.Item>
@@ -79,21 +85,21 @@
           använda {websiteName} för att kunna förbättra innehållet och funktionerna.
         </Accordion.Content>
       </Accordion.Item>
+      <Accordion.Item value="item-4">
+        <Accordion.Trigger
+          >Vad händer när jag får slut på gratis kontaktannonser?</Accordion.Trigger
+        >
+        <Accordion.Content>
+          Förhoppningsvis räcker de {freeCredits} stycken du fick när du skapade
+          ett konto gott och väl för att hitta en bra lärare för dig. Om du mot förmodan
+          får slut på alla {freeCredits} gratis kontaktförfrågningar kan du prenumera
+          på {premiumPlan.name} för {premiumPlan.price}
+          {premiumPlan.priceIntervalName} och kontakta oändligt med lärare.
+          <br />
+          <br />Tycker du att dessa {freeCredits} inte räcker till är du välkommen
+          att <Link href="/contact-us">kontakta oss</Link>.
+        </Accordion.Content>
+      </Accordion.Item>
     </Accordion.Root>
-  </section>
-  <section class="flex w-full flex-col items-center">
-    <SecondaryTitle responsiveMb>Köp fler krediter</SecondaryTitle>
-    <p class="text-muted-foreground">
-      Du kan även köpa fler krediter under <a
-        href="/account/billing"
-        class="link">betalningar</a
-      >.
-    </p>
-    <Button
-      variant="third"
-      on:click={() => goto("/account/billing")}
-      class="mt-2 w-full  md:mt-3 md:w-auto md:min-w-widest"
-      >Köp krediter</Button
-    >
   </section>
 </Container>
