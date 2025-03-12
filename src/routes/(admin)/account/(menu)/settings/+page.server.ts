3235b81b3eab13cea898ace81f114d03a24c3b27
Tarek Bermalm <tarek.bermalm@gmail.com>
2025-03-12 14:36:35 +0100

    fix wrong password error url

--- src/routes/(admin)/account/(menu)/settings/+page.server.ts
+++ src/routes/(admin)/account/(menu)/settings/+page.server.ts
@@ -448,7 +448,7 @@ export const actions = {
       password: current,
     });
 
-    if (error) throw redirect(303, "/settings_password_error");
+    if (error) redirect(303, "/settings-password-error");
     // user was logged out because of bad password. Redirect to error page with explaination.
 
     const { error: updateError } = await supabase.auth.updateUser({
